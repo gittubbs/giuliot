638882a061c71480ef5c615c8c7db3828200655e
Giulio <black.corp.7@gmail.com>
2020-06-07 00:28:18 +0200

    update try catch

--- purifier_reader/hello.py
+++ purifier_reader/hello.py
@@ -12,31 +12,34 @@ token = configReader.fetchToken(configFile,"airpurifier")
 air = AirPurifierMiot(ip,token)
 
 while(True):
-    prop = air.get_properties()
-    dateTimeObj = datetime.now()
+    try:
+        prop = air.get_properties()
+        dateTimeObj = datetime.now()
 
-    
-    #print(prop)
-
-    temp = 0
-    hum = 0
-    aqi = 0
-
-    for e in prop:
-        if(e["did"] == "humidity"):
-            hum = e["value"]
-        if(e["did"] == "temperature"):
-            temp = e["value"]
-        if(e["did"] == "aqi"):
-            aqi = e["value"]
-
-    with open('data.csv', mode = 'a') as csv_file:
-        writer = csv.writer(csv_file, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        #Timestamp, Temperatura, Umidità, AQI
-        writer.writerow([round(time.time() * 1000),temp,hum,aqi])
         
-    print(dateTimeObj)
-    print(temp)
-    print(hum)
-    print(aqi)
-    time.sleep(5.0)
+        #print(prop)
+
+        temp = 0
+        hum = 0
+        aqi = 0
+
+        for e in prop:
+            if(e["did"] == "humidity"):
+                hum = e["value"]
+            if(e["did"] == "temperature"):
+                temp = e["value"]
+            if(e["did"] == "aqi"):
+                aqi = e["value"]
+
+        with open('../visualize/studies/data_3.csv', mode = 'a') as csv_file:
+            writer = csv.writer(csv_file, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            #Timestamp, Temperatura, Umidità, AQI
+            writer.writerow([round(time.time() * 1000),temp,hum,aqi])
+            
+        print(dateTimeObj)
+        print(temp)
+        print(hum)
+        print(aqi)
+        time.sleep(5.0)
+    except Exception:
+        pass
